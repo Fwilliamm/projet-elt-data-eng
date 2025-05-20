7e48d2543c5f54a8d14e8e5a7a6e3035087d3fdb
Fred Kemleu <fkemleu2005@gmail.com>
2025-05-20 15:39:57 +0200

    Automatisation et exécution des pipelines SQL avec Python

---
+++ run_pipeline.py
@@ -0,0 +1,50 @@
+import os
+import logging
+import psycopg2
+from dotenv import load_dotenv
+
+# Chargement des variables d'environnement ===
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+# Fonction d'exécution du fichier sql ===
+def run_sql_file(filename, cursor):
+    logging.info(f"Running {filename}...")
+    with open(filename, "r", encoding="utf-8") as file:
+        sql = file.read()
+        cursor.execute(sql)
+    logging.info(f"{filename} executed successfully.")
+
+# Connexion à PostgreSQL et run des pipelines
+try:
+    connection = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+    connection.autocommit = True
+    cursor = connection.cursor()
+    logging.info("Connected to PostgreSQL.")
+
+    run_sql_file("sql/create_tables.sql", cursor)
+    run_sql_file("sql/insert_dimensions.sql", cursor)
+    run_sql_file("sql/insert_facts.sql", cursor)
+
+    cursor.close()
+    connection.close()
+    logging.info("Pipeline executed and connection closed.")
+
+except Exception as e:
+    logging.error(f"Error during pipeline execution: {e}")
